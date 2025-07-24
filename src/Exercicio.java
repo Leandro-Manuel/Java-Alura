29a4f590fad16956baf645b96336353ea01c6df1
Leandro Manuel Francisco <leandro_00010@outlook.com>
2025-07-23 23:29:57 -0300

    att

---
+++ src/Exercicio.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Exercicio {
+    static String MENSAGEM_MENU = "Ola, bem-vindos!";
+    public static void main(String[] args) {
+        System.out.println(MENSAGEM_MENU);
+        var input = new Scanner(System.in);
+
+        System.out.print("Qual e o seu nome: ");
+        String nome = input.nextLine();
+
+        System.out.print("Qual e a sua idade: ");
+        int idade = input.nextInt();
+
+        boolean verificacao = idade >= 18;
+        String mensagem = verificacao ?
+                nome + ", voce pode dirigir" :
+                nome + ", voce nao pode dirigir";
+
+        System.out.print(mensagem);
+        input.close();
+    }
+}
