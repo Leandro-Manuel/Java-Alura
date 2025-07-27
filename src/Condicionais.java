00903b015d4e29f47cdef719b6f4798729c91046
Leandro Manuel Francisco <leandro_00010@outlook.com>
2025-07-27 19:15:03 -0300

    att

--- src/Condicionais.java
+++ src/Condicionais.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class Condicionais {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         int numero = 10;
         boolean verdade = true;
         if (numero >= 10 && verdade) {
@@ -25,5 +27,57 @@ public class Condicionais {
                 System.out.println("Mês não encontrado.");
         }
         System.out.println(mes);
+
+        System.out.print("Informe um numero de 1 ate 7: ");
+        var option = input.nextInt();
+
+        switch (option) {
+            case 1:
+            case 7:
+                System.out.println("Fim de semana!");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terça");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Sexta");
+                break;
+            default:
+                System.out.println("Opção inválida");
+        }
+
+        var option2 = input.nextInt();
+        switch(option2) {
+            case 1 -> System.out.println("Domingo");
+            case 2 -> System.out.println("Segunda");
+            case 3 -> System.out.println("Terça");
+            case 4 -> System.out.println("Quarta");
+            case 5 -> System.out.println("Quinta");
+            case 6 -> System.out.println("Sexta");
+            case 7 -> System.out.println("Sábado");
+            default -> System.out.println("Opção inválida");
+        }
+
+        var mensagem = switch(option2) {
+            case 1,7 -> {
+                var day = option == 1 ? "Domingo" : "Sabado";
+                yield String.format("Hoje é %s, fim de semana!",day);
+            }
+            case 2 -> "Segunda";
+            case 3 -> "Terça";
+            case 4 -> "Quarta";
+            case 5 -> "Quinta";
+            case 6 -> "Sexta";
+            default -> "Opção inválida";
+        };
     }
 }
