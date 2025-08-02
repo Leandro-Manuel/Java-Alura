0deb4ad12864ce09627302391b7df3bb46425183
Leandro Manuel Francisco <leandro_00010@outlook.com>
2025-08-01 22:59:01 -0300

    polimorfismo

---
+++ src/pooPolimorfismo/Funcionario.java
@@ -0,0 +1,20 @@
+package pooPolimorfismo;
+
+public class Funcionario {
+    protected String nome;
+    protected double salario;
+
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    public void mostrarInformacao() {
+        System.out.printf("Nome do funcionario: %s %nSalário: %.2f",nome,salario);
+    }
+
+    public void reajustarSalario(double porcentagem) {
+        salario += salario * (porcentagem / 100);
+        System.out.printf("%nNovo salário de %s é: R$%.2f",nome,salario);
+    }
+}
